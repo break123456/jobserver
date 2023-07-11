23f873c9f9fdc329630fa17122bb4dcb3322347a
Shalinee <breakinterview@gmail.com>
2023-07-11 23:46:51 +0530

    initial code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+    console.log(`jobserver started at port ${PORT}`);
+});
